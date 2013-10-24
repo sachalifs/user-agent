ebd87811cdfb3adead994558cefe07fac7f889db
slifszyc <sacha.lifszyc@gmail.com>
2013-10-24 18:17:32 -0300

    Add ie.js especially for ie

--- index.js
+++ index.js
@@ -1,58 +1,5 @@
 /**
- * Module dependecies
- */
-
-var inherit = require('inherit');
-var ua = navigator.userAgent.toLowerCase();
-
-/**
  * Expose IE constructor
  */
 
-exports.ie = IE;
-
-/**
- * Creates an IE instance
- */
-
-function IE() {
-  if (!(this instanceof IE)) {
-  	return new IE();
-  }
-  
-  this.name = 'Internet Explorer';
-  this.uaname = 'msie';
-  this.version = ua.indexOf('msie') != -1 ? parseInt(ua.split('msie')[1]) : false;
-}
-
-/**
- * Inherits IE from Query
- */
-
-inherit(IE, Query);
-
-/**
- * Creates Query class
- */
-
-function Query () {}
-
-Query.prototype.lt = function(version) {
-  return this.version && this.version < version;
-};
-
-Query.prototype.lte = function(version) {
-  return this.version && this.version <= version;
-};
-
-Query.prototype.gt = function(version) {
-  return this.version && this.version > version;
-};
-
-Query.prototype.gte = function(version) {
-  return this.version && this.version >= version;
-};
-
-Query.prototype.eq = function(version) {
-  return this.version && this.version == version;
-};
+exports.ie = require('./lib/ie');
