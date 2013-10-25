e11bf389977251a55d12a60755006785d0da9f5e
slifszyc <sacha.lifszyc@gmail.com>
2013-10-25 14:02:49 -0300

    Add Safari support

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 /**
- * Expose IE constructor
+ * Expose browser constructors
  */
 
 exports.ie = require('./lib/ie');
-
-/**
- * Expose FF constructor
- */
-
 exports.ff = require('./lib/ff');
-
-/**
- * Expose Chrome constructor
- */
-
 exports.chrome = require('./lib/chrome');
+exports.safari = require('./lib/safari');
