1b996fb3b2cad189ab82237370e8e41c9044471d
slifszyc <sacha.lifszyc@gmail.com>
2013-10-25 16:25:34 -0300

    Add Mac OS operating system parser

--- index.js
+++ index.js
@@ -6,3 +6,4 @@ exports.ie = require('./lib/ie');
 exports.ff = require('./lib/ff');
 exports.chrome = require('./lib/chrome');
 exports.safari = require('./lib/safari');
+exports.mac = require('./lib/mac');
